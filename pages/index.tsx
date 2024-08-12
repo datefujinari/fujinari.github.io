47e480a6ba672a24ae76436a15dda9b969138fe0
datehujinari <6009.it@gmail.com>
2024-08-13 02:25:11 +0900

    github actions test02

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const Home = () => {
   return (
     <Layout>
-      <h1 className="text-4xl font-bold">My Home Page test</h1>
+      <h1 className="text-4xl font-bold">My Home Page test2</h1>
       {/* プロジェクト一覧をここに表示 */}
     </Layout>
   )
